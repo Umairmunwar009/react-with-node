54477efd8928434e6197382ff08b22f4407808f1
Umair <Umair.m.k009@gmail.com>
2024-11-02 16:27:28 +0500

    update greeting message and specify Node.js and npm versions in package-lock.json

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT=process.env.PORT || 5000;
 // routes handlers 
 app.get('/', (req, res) => {
     console.log("🚀 ~ app.get ~ req:", req)
-  res.send('Hello World');
+  res.send('bye buddy!!');
 });
 app.listen(PORT, () => {
   console.log('Server is running on port 5000');
