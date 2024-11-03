22a5f55c3a568cb306d64fc032c0c03e64c41bfb
Umair <Umair.m.k009@gmail.com>
2024-11-03 22:02:05 +0500

    update root route response message

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT=process.env.PORT || 5000;
 // routes handlers 
 app.get('/', (req, res) => {
     console.log("🚀 ~ app.get ~ req:", req)
-  res.send('bye buddy!!');
+  res.send('Jahayi gui mei kare!!, chai pyanr halao???');
 });
 app.listen(PORT, () => {
   console.log('Server is running on port 5000');
