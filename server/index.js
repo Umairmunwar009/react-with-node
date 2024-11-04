7c078f26b01ca441769744ddd9a15b8b9773d9ce
Umair <Umair.m.k009@gmail.com>
2024-11-04 08:52:27 +0500

    Oauth is added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
-const PORT=process.env.PORT || 5000;
-// routes handlers 
-app.get('/', (req, res) => {
-    console.log("🚀 ~ app.get ~ req:", req)
-  res.send('Jahayi gui mei kare!!, chai pyanr halao???');
-});
+const keys = require("./config/keys");
+const PORT = process.env.PORT || 5000;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleCliendID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log("🚀 ~ accessToken:", accessToken);
+    }
+  )
+);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
 app.listen(PORT, () => {
-  console.log('Server is running on port 5000');
-}); 
+  console.log("Server is running on port 5000");
+});
