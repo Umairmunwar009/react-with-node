9b1d0e007095bf1d77493a05fbfcf1e26a094926
Umair <Umair.m.k009@gmail.com>
2024-11-06 03:51:28 +0500

    add user model and implement Google OAuth authentication

--- server/index.js
+++ server/index.js
@@ -1,28 +1,13 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const app = express();
+const mangoose = require("mongoose");
 const keys = require("./config/keys");
-const PORT = process.env.PORT || 5000;
+const app = express();
+require("./models/User");
+require("./services/passport");
+require("./routes/authRoutes")(app);
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleCliendID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
-    },
-    (accessToken) => {
-      console.log("🚀 ~ accessToken:", accessToken);
-    }
-  )
-);
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"],
-  })
-);
+mangoose.connect(keys.mongooseURI);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log("Server is running on port 5000");
-});
+  console.log(`Server is running on port ${PORT}`);
+}); 
