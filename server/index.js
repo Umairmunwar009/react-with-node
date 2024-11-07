c2479bf2097a2082c4f6c11d41c999ba51d5c62c
Umair <Umair.m.k009@gmail.com>
2024-11-08 03:18:51 +0500

    implement cookie-session and passport for user authentication

--- server/index.js
+++ server/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const mangoose = require("mongoose");
 const keys = require("./config/keys");
 const app = express();
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/User");
 require("./services/passport");
 require("./routes/authRoutes")(app);
-
-mangoose.connect(keys.mongooseURI);
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+mangoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
